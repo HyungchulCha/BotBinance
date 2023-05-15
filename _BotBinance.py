9cc6cf80ce4d179d73941e18ff95e02b84c9ff99
ckgudcjf <ckgudcjf@gmail.com>
2023-05-15 17:00:45 +0900

    ing

--- _BotBinance.py
+++ _BotBinance.py
@@ -64,7 +64,7 @@ class BotBinance():
         self.b_l = list(set(self.q_l + bal_lst))
         self.prc_ttl = prc_ttl if prc_ttl < self.const_up else self.const_up
         self.prc_lmt = prc_lmt if prc_ttl < self.const_up else prc_lmt - (prc_ttl - self.const_up)
-        prc_buy = self.prc_ttl / ((len(self.q_l)) * 0.875)
+        prc_buy = self.prc_ttl / ((len(self.q_l)) * 0.75)
         self.prc_buy = prc_buy if prc_buy > self.const_dn else self.const_dn
 
         line_message(f'BotBinance \nTotal Price : {self.prc_ttl} USDT \nSymbol List : {len(self.b_l)}')
